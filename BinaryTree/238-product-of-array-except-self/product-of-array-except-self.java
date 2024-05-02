31bc7393ee4a4cc6cb3461e38f2b561376b6e0b1
Umang Savla <53813466+umang232@users.noreply.github.com>
2024-05-02 19:09:06 -0400

    Time: 2 ms (80.25%) | Memory: 55.9 MB (9.74%) - LeetSync

---
+++ BinaryTree/238-product-of-array-except-self/product-of-array-except-self.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+        left[0] = 1;
+        int[] ans = new int[nums.length];
+        right[nums.length-1]=1;
+        for(int i=1;i<nums.length;i++){
+            left[i] = left[i-1]*nums[i-1];
+        }
+        for(int j = nums.length-2;j>=0;j--){
+            right[j] = right[j+1]*nums[j+1];
+        }
+        for(int i=0;i<nums.length;i++){
+            ans[i] = left[i]*right[i];
+        }
+        return ans;
+    }
+}
