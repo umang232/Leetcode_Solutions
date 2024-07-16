c213b0cc9b3c9c9fc145cb95f4dc4d87cba9bd76
Umang Savla <53813466+umang232@users.noreply.github.com>
2024-07-16 19:20:55 -0400

    Time: 2 ms (97.91%) | Memory: 44.7 MB (51.66%) - LeetSync

--- BinaryTree/1-two-sum/two-sum.java
+++ BinaryTree/1-two-sum/two-sum.java
@@ -1,16 +1,11 @@
-import java.util.*;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer,Integer> hm = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            hm.put(target-nums[i],i);
-        }
-        for(int i=0;i<nums.length;i++){
-            if(hm.containsKey(nums[i]) && hm.get(nums[i])!=i){
-                int[] ret = new int[2];
-                ret[0] = hm.get(nums[i]);
-                ret[1] = i;
-                return ret;
+        for(int i =0;i<nums.length;i++){
+            if(hm.containsKey(nums[i])){
+                return new int[] {hm.get(nums[i]), i};
+            }else{
+                hm.put(target-nums[i],i);
             }
         }
         return null;
