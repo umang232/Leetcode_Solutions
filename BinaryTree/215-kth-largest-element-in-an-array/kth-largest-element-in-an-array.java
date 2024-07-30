aeb3db3e7a8b0d9421527ffefa0ff7a590709f02
Umang Savla <53813466+umang232@users.noreply.github.com>
2024-07-29 20:05:24 -0400

    Time: 62 ms (35.86%) | Memory: 61.4 MB (21.73%) - LeetSync

---
+++ BinaryTree/215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0;i<nums.length;i++){
+            pq.offer(nums[i]);
+            if(i+1>k){
+                pq.remove();
+            }
+        }
+        return pq.peek();
+    }
+}
