ea8c2b07279d37c648bc9ab618399e766d27f3e9
Umang Savla <53813466+umang232@users.noreply.github.com>
2024-07-29 20:00:37 -0400

    Time: 1 ms (98.79%) | Memory: 40.7 MB (89.26%) - LeetSync

--- BinaryTree/1127-last-stone-weight/last-stone-weight.java
+++ BinaryTree/1127-last-stone-weight/last-stone-weight.java
@@ -3,19 +3,17 @@ class Solution {
         if(stones.length == 1){
             return stones[0];
         }
-        PriorityQueue<Integer> pq= new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for(int i=0;i<stones.length;i++){
             pq.add(stones[i]);
         }
         while(pq.size()>1){
-            int num = pq.poll();
-            int num2 = pq.poll();
-            if(num != num2){
-                pq.add(num-num2);
-            }
+            int val1 = pq.remove();
+            int val2 = pq.remove();
+            pq.add(val1-val2);
         }
-        if(pq.size()>0){
-            return pq.poll();
+        if(pq.size() == 1){
+            return pq.peek();
         }else{
             return 0;
         }
