557176b93307ec267e3b3e47ee1926c78d00f048
Umang Savla <53813466+umang232@users.noreply.github.com>
2024-08-21 17:54:06 -0400

    Time: 88 ms (91.55%) | Memory: 63.6 MB (54.82%) - LeetSync

---
+++ BinaryTree/295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -0,0 +1,47 @@
+class MedianFinder {
+    PriorityQueue<Integer> pqs;
+    PriorityQueue<Integer> pql;
+
+    public MedianFinder() {
+        pqs = new PriorityQueue<>(Collections.reverseOrder());
+        pql = new PriorityQueue<>();
+    }
+    
+    public void addNum(int num) {
+        if(pqs.size() == 0){
+            pqs.add(num);
+        }else if(pqs.size() <= pql.size()){
+            if(num <= pql.peek()){
+                pqs.add(num);
+            }else{
+                pqs.add(pql.remove());
+                pql.add(num);
+            }
+        }else{
+            if(num >= pqs.peek()){
+                pql.add(num);
+            }else{
+                pql.add(pqs.remove());
+                pqs.add(num);
+            }
+        }
+    }
+    
+    public double findMedian() {
+        if(pqs.size() == pql.size()){
+            int sum = pqs.peek()+pql.peek();
+            return (double) sum / 2;
+        }else if(pqs.size() > pql.size()){
+            return pqs.peek();
+        }else{
+            return pql.peek();
+        }
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
