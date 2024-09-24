647ef7e1a1005b42edba482d4e60ba4c6e975f05
Umang Savla <53813466+umang232@users.noreply.github.com>
2024-09-24 18:27:46 -0400

    Time: 1 ms (9.76%) | Memory: 40.6 MB (26.90%) - LeetSync

--- BinaryTree/62-unique-paths/unique-paths.java
+++ BinaryTree/62-unique-paths/unique-paths.java
@@ -1,15 +1,14 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[] r = new int[n];
-        Arrays.fill(r,1);
-        for(int j= 1;j<m;j++){
-            int[] nr = new int[n];
-            Arrays.fill(nr,1);
-            for(int i= n-2;i>=0;i--){
-                nr[i] = nr[i+1] + r[i];
+        int[][] dp = new int[m][n];
+        for (int[] row: dp)
+            Arrays.fill(row, 1);
+        System.out.println(dp[0][0]);
+        for(int i=m-2;i>=0;i--){
+            for(int j = n-2;j>=0;j--){
+                dp[i][j] = dp[i+1][j] + dp[i][j+1]; 
             }
-            r = nr;
         }
-        return r[0];
+        return dp[0][0];
     }
 }
