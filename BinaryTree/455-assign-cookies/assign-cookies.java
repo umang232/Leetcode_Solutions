d85fd2755e8be15500fe92af75d44325f1f7b73d
Umang Savla <53813466+umang232@users.noreply.github.com>
2024-09-26 18:14:13 -0400

    Time: 11 ms (26.13%) | Memory: 48.4 MB (24.90%) - LeetSync

---
+++ BinaryTree/455-assign-cookies/assign-cookies.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count = 0;
+        int j = 0;
+        for(int i =0;i<s.length;i++){
+            if(j<g.length){
+                if(g[j]<=s[i]){
+                    count ++;
+                    j++;
+                }
+            }
+        }
+        return count;
+    }
+}
