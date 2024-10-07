ae5424b50914dd066ca47148b4fbf51522de1f8b
Umang Savla <53813466+umang232@users.noreply.github.com>
2024-10-07 19:11:49 -0400

    Time: 859 ms (5.53%) | Memory: 47.1 MB (47.00%) - LeetSync

---
+++ BinaryTree/1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int left = 0;
+        int maxL = 0;
+        int temp = 0;
+        int a = k;
+        for(int i=0;i<nums.length;i++){
+            
+            // if(nums[i] == 0 && temp == 0){
+            //     left++;
+            // }
+            if(nums[i] == 1){
+                temp ++;
+                maxL = Math.max(maxL,temp);
+            }else if(nums[i] == 0 && a > 0){
+                temp ++;
+                maxL = Math.max(maxL,temp);
+                a--;
+            
+            }else{
+                while(nums[left] != 0){
+                    left++;
+                }
+                
+                left++;
+                temp = (i-left)+1;
+                maxL = Math.max(maxL,temp);
+            }
+            System.out.println(i+" "+nums[i]+" "+temp+" "+a+" "+left);
+        }
+        return Math.max(maxL,temp);
+    }
+}
