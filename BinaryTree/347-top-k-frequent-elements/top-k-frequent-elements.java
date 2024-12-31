053ccd7822cc51f397270a591cf73bf4b39da2a2
Umang Savla <53813466+umang232@users.noreply.github.com>
2024-12-30 21:00:07 -0500

    Time: 11 ms (91.64%) | Memory: 49.6 MB (5.22%) - LeetSync

--- BinaryTree/347-top-k-frequent-elements/top-k-frequent-elements.java
+++ BinaryTree/347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,20 +1,38 @@
+import java.util.*;
+
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        int[] arr = new int[k];
-        for(int i=0;i<nums.length;i++){
-            hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
+        // Step 1: Count frequencies using a HashMap
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int num : nums) {
+            hm.put(num, hm.getOrDefault(num, 0) + 1);
         }
-        PriorityQueue<Map.Entry<Integer,Integer>> pq = new PriorityQueue<>(
-            (a,b) -> b.getValue().compareTo(a.getValue())
-        );
-        pq.addAll(hm.entrySet());
-        int l = 0;
-        for (int i = 0; i < k && !pq.isEmpty(); i++) {
-            arr[l] = pq.poll().getKey();
-            l++;
-        }
-        return arr;
 
+        // Step 2: Create an array of lists to group numbers by frequency
+        List<Integer>[] arr = new ArrayList[nums.length + 1];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = new ArrayList<>();
+        }
+
+        // Populate the array of lists with numbers based on their frequency
+        for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
+            arr[entry.getValue()].add(entry.getKey());
+        }
+
+        // Step 3: Extract the top K frequent elements
+        int[] ans = new int[k];
+        int index = 0;
+
+        // Traverse from the highest frequency to the lowest
+        for (int i = arr.length - 1; i >= 0 && index < k; i--) {
+            for (int num : arr[i]) {
+                ans[index++] = num;
+                if (index == k) {
+                    break; // Stop when we've collected k elements
+                }
+            }
+        }
+
+        return ans;
     }
 }
