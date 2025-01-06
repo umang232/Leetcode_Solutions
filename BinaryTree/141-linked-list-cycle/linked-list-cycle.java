368ad3fb35d0e8c7e3acc18f1581c012ea2be111
Umang Savla <53813466+umang232@users.noreply.github.com>
2025-01-05 19:23:37 -0500

    Time: 0 ms (100.00%) | Memory: 44.4 MB (69.03%) - LeetSync

--- BinaryTree/141-linked-list-cycle/linked-list-cycle.java
+++ BinaryTree/141-linked-list-cycle/linked-list-cycle.java
@@ -18,11 +18,12 @@ public class Solution {
         ListNode fast = head.next;
         
         while(fast!= null && fast.next!=null) {
-            slow = slow.next;
-            fast = fast.next.next;
             if(slow == fast){
                 return true;
             }
+            slow = slow.next;
+            fast = fast.next.next;
+            
         }
         return false;
     }
