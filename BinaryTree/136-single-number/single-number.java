7a9b8bb8018ffa754315bedd9bdcdaa7847af9c5
Umang Savla <53813466+umang232@users.noreply.github.com>
2025-01-15 16:42:15 -0500

    Time: 1 ms (99.89%) | Memory: 46.3 MB (44.07%) - LeetSync

--- BinaryTree/136-single-number/single-number.java
+++ BinaryTree/136-single-number/single-number.java
@@ -1,9 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans = nums[0];
+        int res = nums[0];
         for(int i=1;i<nums.length;i++){
-            ans = ans^nums[i];
+            res = res ^ nums[i];
         }
-        return ans;
+        return res;
     }
 }
