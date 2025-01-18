444499ba863bf30491d35ae0ef23390bb8fccc0e
Umang Savla <53813466+umang232@users.noreply.github.com>
2025-01-18 13:28:09 -0500

    Time: 7 ms (71.31%) | Memory: 42.6 MB (76.04%) - LeetSync

--- BinaryTree/567-permutation-in-string/permutation-in-string.java
+++ BinaryTree/567-permutation-in-string/permutation-in-string.java
@@ -1,39 +1,26 @@
-import java.util.Arrays;
-
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) {
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+        if(s1.length() > s2.length()){
             return false;
         }
-
-        int[] sone1 = new int[26];
-        int[] sone2 = new int[26];
-
-        // Populate the frequency array for s1
-        for (int i = 0; i < s1.length(); i++) {
-            sone1[s1.charAt(i) - 'a']++;
+        for(int i=0;i<s1.length();i++){
+            arr1[s1.charAt(i)-'a']++;
         }
-
-        // Populate the initial window in s2
-        for (int i = 0; i < s1.length(); i++) {
-            sone2[s2.charAt(i) - 'a']++;
+        for(int i=0;i<s1.length();i++){
+            arr2[s2.charAt(i)-'a']++;
         }
-
-        // Check the initial window
-        if (Arrays.equals(sone1, sone2)) {
+        if(Arrays.equals(arr1,arr2)){
             return true;
         }
-
-        // Slide the window across s2
-        for (int j = s1.length(); j < s2.length(); j++) {
-            sone2[s2.charAt(j) - 'a']++; // Add the new character to the window
-            sone2[s2.charAt(j - s1.length()) - 'a']--; // Remove the old character from the window
-
-            if (Arrays.equals(sone1, sone2)) {
+        for(int j=s1.length();j<s2.length();j++){
+            arr2[s2.charAt(j) - 'a']++;
+            arr2[s2.charAt(j-s1.length())-'a']--;
+            if(Arrays.equals(arr1,arr2)){
                 return true;
             }
         }
-
         return false;
     }
 }
