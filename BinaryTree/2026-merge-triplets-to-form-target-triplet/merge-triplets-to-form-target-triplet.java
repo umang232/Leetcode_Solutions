dcfcb438ad5ae85f58aa522166f98d88c4e82ae7
Umang Savla <53813466+umang232@users.noreply.github.com>
2025-02-09 12:26:09 -0500

    Time: 1 ms (97.85%) | Memory: 121.2 MB (46.63%) - LeetSync

--- BinaryTree/2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
+++ BinaryTree/2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean mergeTriplets(int[][] triplets, int[] target) {
         int count1 = 0;
-        int count2 =0;
-        int count3 =0;
+        int count2 = 0;
+        int count3 = 0;
         for(int i=0;i<triplets.length;i++){
-            if(triplets[i][0]<=target[0] && triplets[i][1] <= target[1] && triplets[i][2] <= target[2]){
+            if(triplets[i][0] <= target[0] && triplets[i][1] <= target[1] && triplets[i][2] <= target[2]){
                 if(triplets[i][0] == target[0]) count1 =1;
-                if(triplets[i][1] == target[1]) count2 = 1;
-                if(triplets[i][2] == target[2]) count3 = 1;
-                if((count1 == 1) && (count2 == 1) && (count3 == 1)){
+                if(triplets[i][1] == target[1]) count2 =1;
+                if(triplets[i][2] == target[2]) count3 =1;
+                if(count1 == 1 && count2 ==1 && count3 == 1){
                     return true;
                 }
             }
