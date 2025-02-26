d65c0a56fdf2ad9cdd9f2ca17a346a1c3f06a367
Umang Savla <53813466+umang232@users.noreply.github.com>
2025-02-26 13:05:36 -0500

    Time: 34 ms (76.57%) | Memory: 55.5 MB (55.91%) - LeetSync

--- BinaryTree/208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ BinaryTree/208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,49 +1,42 @@
 class Trie {
-
     Node root;
+
     public Trie() {
         root = new Node('\0');
     }
     
     public void insert(String word) {
         Node cur = root;
-        for(char x : word.toCharArray()){
-            if(cur.children[x - 'a'] == null){
-                cur.children[x-'a'] = new Node(x);
+        for(char c : word.toCharArray()){
+            if(cur.children[c-'a'] == null){
+                cur.children[c-'a'] = new Node(c);
             }
-            cur = cur.children[x-'a'];
+            cur = cur.children[c-'a'];
         }
         cur.isWord = true;
-        
     }
     
     public boolean search(String word) {
         Node cur = root;
-        for(char x : word.toCharArray()){
-            if(cur.children[x-'a']== null){
+        for(char x: word.toCharArray()){
+            if(cur.children[x-'a'] == null){
                 return false;
             }
             cur = cur.children[x-'a'];
         }
-        if(cur.isWord == true){
-            return true;
-        }else{
-            return false;
-        }
-        
+        return cur.isWord;
     }
     
     public boolean startsWith(String prefix) {
         Node cur = root;
-        for(char x : prefix.toCharArray()){
-            if(cur.children[x-'a']== null){
+        for(char x: prefix.toCharArray()){
+            if(cur.children[x-'a'] == null){
                 return false;
             }
             cur = cur.children[x-'a'];
         }
         return true;
     }
-
     class Node{
         private char value;
         private boolean isWord;
