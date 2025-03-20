9a1b54f33be797e81dcb58acd73a43e43400cbbe
Umang Savla <53813466+umang232@users.noreply.github.com>
2025-03-19 22:40:53 -0400

    Time: 0 ms (100.00%) | Memory: 41.9 MB (50.42%) - LeetSync

--- BinaryTree/63-unique-paths-ii/unique-paths-ii.java
+++ BinaryTree/63-unique-paths-ii/unique-paths-ii.java
@@ -1,43 +1,38 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-        int[][]dp = new int[m][n];
-        if (obstacleGrid[m-1][n-1] == 1 || obstacleGrid[0][0] == 1) {
-            return 0;
+        int r = obstacleGrid.length;
+        int c = obstacleGrid[0].length;
+        int[][]dp = new int[r][c];
+        if(obstacleGrid[r-1][c-1] == 1){
+            dp[r-1][c-1] = 0;
         }
-
-        // Initialize the bottom-right corner
-        dp[m-1][n-1] = 1;
-
-        // Initialize the last row
-        for (int i = n - 2; i >= 0; i--) {
-            if (obstacleGrid[m-1][i] == 1) {
-                dp[m-1][i] = 0; // obstacle, no path
-            } else {
-                dp[m-1][i] = dp[m-1][i+1]; // inherit value from the right
+        else{
+            dp[r-1][c-1] = 1;
+        }
+        for(int i= c-2; i>=0;i--){
+            if(obstacleGrid[r-1][i] != 1){
+                dp[r-1][i] = dp[r-1][i+1]; 
+            }else{
+                dp[r-1][i] = 0;
             }
         }
-
-        // Initialize the last column
-        for (int i = m - 2; i >= 0; i--) {
-            if (obstacleGrid[i][n-1] == 1) {
-                dp[i][n-1] = 0; // obstacle, no path
-            } else {
-                dp[i][n-1] = dp[i+1][n-1]; // inherit value from the bottom
+        for(int i= r-2; i>=0;i--){
+            if(obstacleGrid[i][c-1] != 1){
+                dp[i][c-1] = dp[i+1][c-1]; 
+            }else{
+                dp[i][c-1] = 0;
             }
         }
-
-       for (int i = m - 2; i >= 0; i--) {
-            for (int j = n - 2; j >= 0; j--) {
-                if (obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0; // obstacle, no path
-                } else {
-                    dp[i][j] = dp[i+1][j] + dp[i][j+1]; // sum from bottom and right
+        for(int i=r-2;i>=0;i--){
+            for(int j = c-2;j>=0;j--){
+                if(obstacleGrid[i][j] == 0){
+                    dp[i][j] = dp[i+1][j] + dp[i][j+1];
+                }else{
+                    dp[i][j] = 0;
                 }
             }
         }
 
-        return dp[0][0]; // The top-left corner will have the answer
+        return dp[0][0];
     }
 }
