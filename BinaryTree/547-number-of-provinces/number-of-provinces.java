91f93ed888d72c1eb2441ca689fd86bfdebdad84
Umang Savla <53813466+umang232@users.noreply.github.com>
2025-03-27 13:01:02 -0400

    Time: 2 ms (48.90%) | Memory: 47.5 MB (69.52%) - LeetSync

---
+++ BinaryTree/547-number-of-provinces/number-of-provinces.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        List<List<Integer>> adjMatrix = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adjMatrix.add(new ArrayList<>());
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1 && i != j) {     
+                    adjMatrix.get(i).add(j);
+                }
+            }
+        }
+
+        boolean[] visited = new boolean[n];
+        int count = 0;
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                dfs(visited, i, adjMatrix);
+                count++;
+            }
+            
+        }
+        return count;
+    }
+
+    private void dfs(boolean[] visited,int i,List<List<Integer>> adjMatrix){
+        if(visited[i]){
+            return;
+        }
+        visited[i] = true;
+        for(int nei : adjMatrix.get(i)){
+            if(!visited[nei]){
+                dfs(visited,nei,adjMatrix);
+            }
+        }
+    }
+}
