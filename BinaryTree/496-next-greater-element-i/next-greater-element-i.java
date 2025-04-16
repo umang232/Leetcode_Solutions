a8f14f69afffa0aadceac45af484fff0d814a65c
Umang Savla <53813466+umang232@users.noreply.github.com>
2025-04-15 20:22:23 -0400

    Time: 4 ms (36.99%) | Memory: 45.1 MB (6.84%) - LeetSync

--- BinaryTree/496-next-greater-element-i/next-greater-element-i.java
+++ BinaryTree/496-next-greater-element-i/next-greater-element-i.java
@@ -1,27 +1,21 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(nums2[nums2.length-1], -1);
         Stack<Integer> st = new Stack<>();
-        for(int i = 0;i<nums2.length;i++){
-            if(st.isEmpty()){
-                st.push(nums2[i]);
-            }else if(nums2[i] > st.peek()){
-                while(!st.isEmpty() && st.peek() < nums2[i]){
-                    map.put(st.pop(),nums2[i]);
-                }
-                st.push(nums2[i]);
-            }else{
-                st.push(nums2[i]);
+        st.push(nums2[nums2.length-1]);
+        for(int i= nums2.length - 2;i>=0;i--){
+            while(!st.isEmpty() && st.peek() <= nums2[i]){
+                st.pop();
             }
+            int ele = st.isEmpty() ? -1 : st.peek();
+            map.put(nums2[i],ele);
+            st.push(nums2[i]);
         }
-        System.out.println(map);
-        int ans [] = new int[nums1.length];
-        Arrays.fill(ans,-1);
-        for(int i=0;i<nums1.length;i++){
-            if(map.containsKey(nums1[i])){
-                ans[i] = map.get(nums1[i]);
-            }
+        int[] res = new int[nums1.length];
+        for(int i=0;i<res.length;i++){
+            res[i] = map.get(nums1[i]);
         }
-        return ans;
+        return res;
     }
 }
