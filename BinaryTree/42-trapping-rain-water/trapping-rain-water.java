dc32ef63a5f9e67e38de3ba7c3353816329d76c2
Umang Savla <53813466+umang232@users.noreply.github.com>
2025-04-17 17:22:13 -0400

    Time: 0 ms (100.00%) | Memory: 47 MB (8.30%) - LeetSync

--- BinaryTree/42-trapping-rain-water/trapping-rain-water.java
+++ BinaryTree/42-trapping-rain-water/trapping-rain-water.java
@@ -1,31 +1,26 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
-        if (n == 1 || n == 2) {
+        if(height.length == 1 || height.length == 2){
             return 0;
         }
         int water = 0;
         int leftmax = height[0];
-        int rightmax = height[n - 1];
+        int rightmax = height[height.length-1];
         int i = 1;
-        int j = n - 2;
-        while (i <= j) {
-            if (leftmax <= rightmax) {
-                if (height[i] >= leftmax) {
+        int j = height.length - 2;
+        while(i<=j){
+            if(leftmax <= rightmax){
+                if(height[i] >= leftmax){
                     leftmax = height[i];
-                } else {
-                    if ((leftmax - height[i]) > 0) {
-                        water += (leftmax - height[i]);
-                    }
+                }else{
+                    water += (leftmax - height[i]);
                 }
                 i++;
-            } else {
-                if (height[j] >= rightmax) {
+            }else{
+                if(height[j] >= rightmax){
                     rightmax = height[j];
-                } else {
-                    if ((rightmax - height[j]) > 0) {
-                        water += (rightmax - height[j]);
-                    }
+                }else{
+                    water += (rightmax-height[j]);
                 }
                 j--;
             }
