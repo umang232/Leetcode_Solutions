b0328209b5518a21c9f3c2eabfd4744b60963048
Umang Savla <53813466+umang232@users.noreply.github.com>
2025-04-17 16:42:44 -0400

    Time: 3 ms (85.21%) | Memory: 49.8 MB (45.45%) - LeetSync

--- BinaryTree/200-number-of-islands/number-of-islands.java
+++ BinaryTree/200-number-of-islands/number-of-islands.java
@@ -4,21 +4,21 @@ class Solution {
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j] == '1'){
-                    dfs(grid,i,j);
+                    dfs(grid, i, j);
                     count++;
                 }
             }
         }
         return count;
     }
-    public void dfs(char[][]grid,int i,int j){
-        if(i < 0 || j>grid[0].length-1 || i > grid.length-1 || j < 0 || grid[i][j] == '0'){
+    public void dfs(char[][]grid, int i, int j){
+        if(i < 0 || i > grid.length-1 || j < 0 || j> grid[0].length - 1 || grid[i][j] == '0'){
             return;
         }
         grid[i][j] = '0';
-        dfs(grid,i-1,j);
-        dfs(grid,i+1,j);
-        dfs(grid, i, j-1);
-        dfs(grid, i , j+1);
+        dfs(grid, i-1, j);
+        dfs(grid, i+1 ,j);
+        dfs(grid, i , j-1);
+        dfs(grid, i, j + 1);
     }
 }
