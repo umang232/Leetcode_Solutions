6ce1ef2f826f45b0029fc6216d6292d7e2d22807
Umang Savla <53813466+umang232@users.noreply.github.com>
2025-04-18 19:36:01 -0400

    Time: 0 ms (100.00%) | Memory: 42.5 MB (75.03%) - LeetSync

--- BinaryTree/21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ BinaryTree/21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -1,53 +1,22 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1 == null){
-            return list2;
-        }
-        if(list2 == null){
-            return list1;
-        }
-        ListNode temp1 = list1;
-        ListNode temp2 = list2;
-        ListNode temp3 = null;
-        if(temp1.val < temp2.val){
-            temp3 = temp1;
-            temp1 = temp1.next;
-        }
-        else{
-            temp3 = temp2;
-            temp2 = temp2.next;
-        }
-       ListNode ret = temp3; 
-        while(temp1 != null && temp2 != null){
-            if(temp1.val < temp2.val){
-                temp3.next = temp1;
-                temp1 = temp1.next;
-            }else{
-                temp3.next = temp2;
-                temp2 = temp2.next;
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
             }
-            temp3 = temp3.next;
+            current = current.next;
         }
-        while(temp1!=null){
-            temp3.next = temp1;
-            temp1 = temp1.next;
-            temp3 = temp3.next;
-        }
-        while(temp2!=null){
-            temp3.next = temp2;
-            temp2 = temp2.next;
-            temp3 = temp3.next;
-        }
-        return ret;
+
+        // Attach remaining nodes (only one of them is non-null)
+        current.next = (list1 != null) ? list1 : list2;
+
+        return dummy.next;
     }
 }
