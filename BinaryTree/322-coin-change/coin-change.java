0b38a0c8cc6c85e555d490fbfaa5fa9b2617b914
Umang Savla <53813466+umang232@users.noreply.github.com>
2025-04-19 13:55:25 -0400

    Time: 14 ms (85.53%) | Memory: 44.6 MB (57.23%) - LeetSync

--- BinaryTree/322-coin-change/coin-change.java
+++ BinaryTree/322-coin-change/coin-change.java
@@ -1,19 +1,17 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount+1];
-        Arrays.fill(dp,-1);
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1); // Initialize with a large value
         dp[0] = 0;
-        for(int i=1;i<=amount;i++){
-            for(int coin : coins){
-                if(coin <= i){
-                    if(dp[i] == -1 && dp[i-coin] != -1){
-                        dp[i] = 1 + dp[i-coin];
-                    }else{
-                        if(dp[i-coin] != -1) dp[i] = Math.min(dp[i], 1+dp[i-coin]);
-                    }
+
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
-        return dp[amount];
+
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
