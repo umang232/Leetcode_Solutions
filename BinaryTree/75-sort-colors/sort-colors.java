e8f5589a0eb45cbf5cde5ebc987ef11800345156
Umang Savla <53813466+umang232@users.noreply.github.com>
2025-04-20 13:19:27 -0400

    Time: 0 ms (100.00%) | Memory: 42.4 MB (10.22%) - LeetSync

--- BinaryTree/75-sort-colors/sort-colors.java
+++ BinaryTree/75-sort-colors/sort-colors.java
@@ -1,19 +1,20 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int i =0, j =0,k = nums.length-1;
-        while(j <= k){
+        int i=0;
+        int j = 0;
+        int k = nums.length-1;
+        while(j<=k){
             if(nums[j] == 0){
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
                 i++;
                 j++;
             }else if(nums[j] == 2){
-                int temp = nums[k];
-                nums[k] = nums[j];
-                nums[j] = temp;
+                int temp = nums[j];
+                nums[j] = nums[k];
+                nums[k] = temp;
                 k--;
-                
             }else{
                 j++;
             }
