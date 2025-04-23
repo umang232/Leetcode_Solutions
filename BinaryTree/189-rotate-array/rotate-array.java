55b2b85c6a8c66ea8b05ef27aa61f75dbcb3cfa5
Umang Savla <53813466+umang232@users.noreply.github.com>
2025-04-23 11:17:32 -0400

    Time: 0 ms (100.00%) | Memory: 57.4 MB (65.91%) - LeetSync

--- BinaryTree/189-rotate-array/rotate-array.java
+++ BinaryTree/189-rotate-array/rotate-array.java
@@ -1,36 +1,20 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        k = k %nums.length;
-        if(k == 0){
-            return;
-        }
-        int i =0;
-        int j = nums.length-k-1;
-        while(i<j){
-            int temp=nums[i];
-            nums[i]=nums[j];
-            nums[j] = temp;
-            i++;
-            j--;
-        }
-        i= nums.length-k;
-        j = nums.length-1;
-        while(i<j){
-            int temp=nums[i];
-            nums[i]=nums[j];
-            nums[j] = temp;
-            i++;
-            j--;
-        }
-        i=0;
-        j=nums.length-1;
-        while(i<j){
-            int temp=nums[i];
-            nums[i]=nums[j];
-            nums[j] = temp;
-            i++;
-            j--;
-        }
+        int n = nums.length;
+        k = k % n;
 
+        reverse(nums, 0, n - 1);    
+        reverse(nums, 0, k - 1);         
+        reverse(nums, k, n - 1);         
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
     }
 }
