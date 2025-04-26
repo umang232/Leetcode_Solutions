fdd04943c1ea89baac637cf081f4235452cc2141
Umang Savla <53813466+umang232@users.noreply.github.com>
2025-04-26 13:30:08 -0400

    Time: 0 ms (100.00%) | Memory: 42 MB (36.14%) - LeetSync

--- BinaryTree/1894-merge-strings-alternately/merge-strings-alternately.java
+++ BinaryTree/1894-merge-strings-alternately/merge-strings-alternately.java
@@ -2,19 +2,19 @@ class Solution {
     public String mergeAlternately(String word1, String word2) {
         int i = 0;
         int j = 0;
-        String result = "";
+        StringBuilder result = new StringBuilder();
         while(i != word1.length() && j != word2.length()){
-            result += word1.charAt(i);
-            result+= word2.charAt(j);
+            result.append(word1.charAt(i));
+            result.append(word2.charAt(j));
             i++;
             j++;
         }
         if(i != word1.length()){
-            result += word1.substring(i);
+            result.append(word1.substring(i));
         }
         if( j != word2.length()){
-            result+= word2.substring(j);
+            result.append(word2.substring(j));
         }
-        return result;
+        return result.toString();
     }
 }
