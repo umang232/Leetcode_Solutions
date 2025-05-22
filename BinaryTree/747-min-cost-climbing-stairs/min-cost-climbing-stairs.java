3257a8a0a124c2743eeb183e1620fa5d4b948300
Umang Savla <53813466+umang232@users.noreply.github.com>
2025-05-22 15:08:28 +0530

    Time: 1 ms (38.69%) | Memory: 43.2 MB (63.59%) - LeetSync

--- BinaryTree/747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ BinaryTree/747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -3,11 +3,9 @@ class Solution {
         if(cost.length == 2){
             return Math.min(cost[0],cost[1]);
         }
-        int dp [] = new int[cost.length+1];
-        dp[0] = 0;
-        dp[1] = 0;
-        for(int i=2;i<cost.length+1;i++){
-            dp[i] = Math.min(cost[i-1]+dp[i-1],cost[i-2]+dp[i-2]);
+        int[] dp = new int[cost.length+1];
+        for(int i=2;i<=cost.length;i++){
+            dp[i] = Math.min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2]);
         }
         return dp[cost.length];
     }
